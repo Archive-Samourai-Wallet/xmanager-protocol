ccae75c63b1782a9b82be9768961f60fefa99f6d
zeroleak <zeroleak@samourai.io>
2020-02-03 11:34:05 +0100

    update testnet addresses

--- src/main/java/com/samourai/xmanager/protocol/XManagerService.java
+++ src/main/java/com/samourai/xmanager/protocol/XManagerService.java
@@ -2,13 +2,13 @@ package com.samourai.xmanager.protocol;
 
 public enum XManagerService {
   WHIRLPOOL(
-      "bc1qxya59zn6fgenfls0pedt0xqkagd33fcfc5s04n", "tb1qcfgn9nlgxu0ycj446prdkg0p36qy5a39pcf74v"),
-  SAAS("bc1qsnefhrmd8gs6a4j7fzxfy4ay52j78agl6rsw36", "tb1q6m3urxjc8j2l8fltqj93jarmzn0975nnxuymnx"),
+      "bc1qxya59zn6fgenfls0pedt0xqkagd33fcfc5s04n", "tb1q6m3urxjc8j2l8fltqj93jarmzn0975nnxuymnx"),
+  SAAS("bc1qsnefhrmd8gs6a4j7fzxfy4ay52j78agl6rsw36", "tb1q2ek9xn5zh6lcl4cjemywkjpnhvgl8w7qucm29u"),
   RICOCHET(
-      "bc1qptv26ag03um6dhvcrfjh29wlez64mdufw77z8g", "tb1q6y29zmgw2ajgmqrnpqrf0ejtyrqsh3ehd6wjlk"),
-  BIP47("bc1quq8h89h6j9f6nv5fyqjjav2auzah799lq2j9fa", "tb1q6y29zmgw2ajgmqrnpqrf0ejtyrqsh3ehd6wjlk"),
+      "bc1qptv26ag03um6dhvcrfjh29wlez64mdufw77z8g", "tb1q2ek9xn5zh6lcl4cjemywkjpnhvgl8w7qucm29u"),
+  BIP47("bc1quq8h89h6j9f6nv5fyqjjav2auzah799lq2j9fa", "tb1q2ek9xn5zh6lcl4cjemywkjpnhvgl8w7qucm29u"),
   OXT_RESEARCH(
-      "bc1qj2sac3nvaettzz5egmtnk223vjrey2hxyx9j9k", "tb1q6y29zmgw2ajgmqrnpqrf0ejtyrqsh3ehd6wjlk");
+      "bc1qj2sac3nvaettzz5egmtnk223vjrey2hxyx9j9k", "tb1q2ek9xn5zh6lcl4cjemywkjpnhvgl8w7qucm29u");
 
   private String defaultAddressMainnet;
   private String defaultAddressTestnet;
